1b093334dffe0b197e26160d4e34e478aaeaae64
Devon <Devon.Hartlen@uwaterloo.ca>
2022-04-13 15:00:40 -0400

    unit tests for polyxpoly

--- tests/test_PolygonFunctions.py
+++ tests/test_PolygonFunctions.py
@@ -17,6 +17,7 @@ def test_signedpolyarea():
     assert np.sign(area) == 1
     # Test that area is 1
     assert np.abs(area) == 1
+    print('   test_signedpolyarea')
     print('CW Area = %f' %area)
     
     # Define a counter-clockwise 1x1 square
@@ -71,7 +72,7 @@ def test_polyarea():
     ])
 
     area = poly.polyarea(xyccw)
-
+    print('   test_polyarea')
     print('Computed Polygon Area %f' %area)
 
     assert pytest.approx(area) == 5.0
@@ -90,6 +91,7 @@ def test_segintersect():
         [1, 0]
     ])
     intersect = poly.segintersect(xy1, xy2)
+    print('   test_segintersect')
     print('case 1: intersecting = {0}'.format(intersect))
     assert (intersect == True)
 
@@ -181,10 +183,75 @@ def test_inpolygon():
     # Case 14: collinear with polygon segment, but outside. assert true
     assert (poly.inpolygon(xyccw, [3, 3]) == False)
     
+def test_polyxpoly_functions():
+    """
+    Testing polyxpoly with functions
+    """
+    # Parametric curves taken from github.com/sukhbinder/intersection.
+    # Methodology is not taken from the same source. 
+    a, b = 1, 2
+    phi = np.linspace(3, 10, 100)
+    x1 = a*phi - b*np.sin(phi)
+    y1 = a - b*np.cos(phi)
+    poly1 = np.column_stack((x1, y1))
+    x2 = phi
+    y2 = np.sin(phi)+2
+    poly2 = np.column_stack((x2, y2))
+
+    intervals, interinds = poly.polyxpoly(poly1, poly2)
+    print('   test_polyxpoly_functions')
+    print(intervals)
+    print(interinds)
+
+    assert pytest.approx(intervals[:,0]) == np.array([6.10765984, 8.36483107])
+    assert pytest.approx(intervals[:,1]) == np.array([1.82539714, 2.87208714])
+
+def test_polyxpoly_polyline():
+    """
+    Testing polyxpoly with a closed polygon and line. 
+    Intercepts validated using MATLAB polyxpoly()
+    """
+
+    polygon = np.array([
+        [0, 0, 1, 3, 4, 5, 7, 8, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        [0, 1, 3, 4, 5, 4, 4, 3, 2, 1, 0, 1, 2, 3, 3, 2, 2, 1]])
+    polygon = polygon.T
+    line = np.array([[0, 2, 6, 9],[-1, 5, 3, 0]])
+    line = line.T
+
+    # Case 1: test open polygon
+    intervals, interinds = poly.polyxpoly(polygon, line)
+    print('   test_polyxpoly_polyline')
+    print('--- Case 1 ---')
+    print(intervals)
+    print(interinds)
+    assert pytest.approx(intervals[:,0]) == np.array([1.4, 3.333333, 8.5])
+    assert pytest.approx(intervals[:,1]) == np.array([3.2, 4.333333, 0.5])
+
+    # Case 2: Test with closed polygon
+    polygon = np.vstack((polygon, polygon[0,:]))
+    intervals, interinds = poly.polyxpoly(polygon, line)
+    print('--- Case 2 ---')
+    print(intervals)
+    print(interinds)
+    assert pytest.approx(intervals[:,0]) == np.array([1.4, 3.333333, 8.5, 0.5])
+    assert pytest.approx(intervals[:,1]) == np.array([3.2, 4.333333, 0.5, 0.5])
+
+    # Case 3: Ensure things work for reverse rotation polygon
+    polygon = np.flipud(polygon)
+    intervals, interinds = poly.polyxpoly(polygon, line)
+    print('--- Case 3 ---')
+    print(intervals)
+    print(interinds)
+    assert pytest.approx(intervals[:,0]) == np.array([0.5, 8.5, 3.333333, 1.4])
+    assert pytest.approx(intervals[:,1]) == np.array([0.5, 0.5, 4.333333, 3.2])
+
 
 if __name__ == '__main__':
-    # test_signedpolyarea()
-    # test_ispolycw()
-    # test_polyarea()
-    # test_segintersect()
+    test_signedpolyarea()
+    test_ispolycw()
+    test_polyarea()
+    test_segintersect()
     test_inpolygon()
+    test_polyxpoly_functions()
+    test_polyxpoly_polyline()
