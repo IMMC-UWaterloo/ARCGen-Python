5b7ba16bf21ad0cb524be372bf6ec647b179f66b
Devon <Devon.Hartlen@UWaterloo.ca>
2022-04-13 21:31:45 -0400

    First Version of uniquetol
    Seems to work, but needs work. I think I can reduce evaluations quite a bit.

--- uniquetol/uniquetol.py
+++ uniquetol/uniquetol.py
@@ -1,3 +1,4 @@
+from enum import unique
 import numpy as np
 import numpy.typing as npt
 
@@ -12,61 +13,76 @@ def uniquetol(a:npt.ArrayLike, tol=1e-6):
     a = nx2 numpy array
     tol = absolute tolerance
     """
-    col1 = a[:,0]
-    col2 = a[:,1]
-
-    col1sortind = np.argsort(col1)
-    col2sortind = np.argsort(col2)
-
-    print(col1sortind)
-    print(col2sortind)
-
-    sortedcol1 = col1[col1sortind]
-    sortedcol2 = col2[col2sortind]
-
-    difcol1 = np.diff(sortedcol1)
-    difcol2 = np.diff(sortedcol2)
-
-    samecol1 = difcol1 < tol
-    samecol2 = difcol2 < tol
-    
-    samecol1 = np.hstack((np.asarray(0), samecol1))
-    samecol2 = np.hstack((0, samecol2))
-    
-
-    print(samecol1)
-    print(samecol2)
-
-    dupIndex = np.ones((samecol1.shape[0]))
-    dupIndex = dupIndex.astype(bool)
-
-    for i, index1 in enumerate(col1sortind):
-        index2 = np.argwhere(col2sortind == index1)
-        if (samecol1[index1] == 1) and (samecol2[index2] == 1):
-            dupIndex[i] = 0
-            print('row {0} (indices {1}, {2}) is a duplicate'.format(index1, index1, index2))
-
-    print(dupIndex)
-    outputarray = a[col1sortind,:]
+    # Algorithm approach: MATLAB mentions a "lexoconographical approach"
+    # 1) Sort first column low high
+    # 2) loop through first column. find equal entries
+    # 3) for equal entries, sort second column
+    print('before sort')
     print(a)
-    outputarray = outputarray[dupIndex,:]
+    
+    # start by sorting array based on first column
+    sortInd = np.argsort(a[:,0],axis=0)
+    a = a[sortInd,:]
 
-    print(outputarray)
+    print('after sort')
+    print(a)
 
+    # Now, for each set of matched (exact matched) first column entries, sort 
+    # the second column and swap as needed. Do not alter second column
+    i = 0
+    while i < a.shape[0]:
+        index = (a[i:,0] == a[i,0])
+        index = np.flatnonzero(index == True)
+        if (len(index) > 1):
+            subset = a[i+index,:]
+            subsetIndex = np.argsort(subset[:,1], axis=0)
+            a[i:i+len(index),:] = subset[subsetIndex,:]
+            i += len(index)
+
+        else:    
+            i += 1
+    
+    # We now have a lexoconographic sort of a
+    print(a)
+
+    # Culling: initialize a "saveindex" that will mark which rows of a are to 
+    # be saved. Then, go through a. Look ahead to find any rows within tolerance
+    # and mark those to be removed. Only keep first entry. 
+    saveIndex = np.ones((a.shape[0]))
+    for i in range(a.shape[0]):
+        if saveIndex[i] == 0:
+            continue
+        else:
+            # I can probably replace this with vectorized math
+            for j in range(i+1, a.shape[0]):
+                if (np.max(np.abs( a[i,:] - a[j,:] )) < tol):
+                    saveIndex[j] = 0
+    
+    # Now cull array
+    a = a[saveIndex.astype(bool),:]
+    print('Culled Array')
+    print(a)
+
+    # why do I need this second set of loops? Can I just do my culling in the 
+    # first sort?
 
 
 if __name__ == '__main__':
     a = np.array([
-        [0,0],
-        [1, 1.0000001],
+        [1, 1+1e-5],
+        [2-1e-5, 2+1e-5],
         [1,1], 
-        [1.0000001, 2],
-        [0.999999, 2],
-        [1,2]
+        [2+1e-5, 2],
+        [1+1e-5, 2],
+        [2, 1],
+        [0,0], 
+        [1,2+1e-5],
+        [2-1e-5, 1],
+        [2, 2],
     ])
 
     # uniquetol(a, 1e-4)
-    print(np.isclose(a))
+    uniquetol(a, 1e-4)
 
 
 
