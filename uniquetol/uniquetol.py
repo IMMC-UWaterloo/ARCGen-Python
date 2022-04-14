6e9f81d72bd1c6e7ec4163c72b4d21f01b63fdbc
Devon <Devon.Hartlen@uwaterloo.ca>
2022-04-14 10:29:36 -0400

    Rewrote uniquetol
    runs about twice as quick as previous version, but is still pretty slow. 
    
    Needs a test with a lot of row entries to really assess performance

--- uniquetol/uniquetol.py
+++ uniquetol/uniquetol.py
@@ -1,6 +1,7 @@
-from enum import unique
+
 import numpy as np
 import numpy.typing as npt
+import timeit
 
 def uniquetol(a:npt.ArrayLike, tol=1e-6):
     """
@@ -13,76 +14,93 @@ def uniquetol(a:npt.ArrayLike, tol=1e-6):
     a = nx2 numpy array
     tol = absolute tolerance
     """
-    # Algorithm approach: MATLAB mentions a "lexoconographical approach"
+        # Algorithm approach: MATLAB mentions a "lexoconographical approach"
     # 1) Sort first column low high
-    # 2) loop through first column. find equal entries
-    # 3) for equal entries, sort second column
-    print('before sort')
-    print(a)
+    # 2) for each row, find all subsequent rows within tolerance and cull
+    # This is an expensive appraoch, but robust. 
     
     # start by sorting array based on first column
     sortInd = np.argsort(a[:,0],axis=0)
     a = a[sortInd,:]
 
-    print('after sort')
-    print(a)
-
-    # Now, for each set of matched (exact matched) first column entries, sort 
-    # the second column and swap as needed. Do not alter second column
-    i = 0
-    while i < a.shape[0]:
-        index = (a[i:,0] == a[i,0])
-        index = np.flatnonzero(index == True)
-        if (len(index) > 1):
-            subset = a[i+index,:]
-            subsetIndex = np.argsort(subset[:,1], axis=0)
-            a[i:i+len(index),:] = subset[subsetIndex,:]
-            i += len(index)
-
-        else:    
-            i += 1
-    
-    # We now have a lexoconographic sort of a
-    print(a)
-
-    # Culling: initialize a "saveindex" that will mark which rows of a are to 
-    # be saved. Then, go through a. Look ahead to find any rows within tolerance
-    # and mark those to be removed. Only keep first entry. 
-    saveIndex = np.ones((a.shape[0]))
+    # Going row-by-row, search ahead for rows within tolerance
+    # Probably a way to window this method for faster execution.
+    saveindex = np.ones((a.shape[0]))
     for i in range(a.shape[0]):
-        if saveIndex[i] == 0:
+        # if row is already culled, skip
+        if saveindex[i] == 0:
             continue
+        # Find all rows within maximum tolerance
         else:
-            # I can probably replace this with vectorized math
-            for j in range(i+1, a.shape[0]):
-                if (np.max(np.abs( a[i,:] - a[j,:] )) < tol):
-                    saveIndex[j] = 0
-    
-    # Now cull array
-    a = a[saveIndex.astype(bool),:]
-    print('Culled Array')
-    print(a)
+            # within tol is when difference in both columns is below tol
+            withintol = np.max(np.abs(a[i,:] - a[i+1:, :]) ,axis=1) < tol
+            withintol = np.hstack((np.zeros(i+1).astype(bool), withintol))
+            saveindex[withintol.astype(bool)] = 0
 
-    # why do I need this second set of loops? Can I just do my culling in the 
-    # first sort?
+    a = a[saveindex.astype(bool)]
+
+    return a
 
 
 if __name__ == '__main__':
-    a = np.array([
-        [1, 1+1e-5],
-        [2-1e-5, 2+1e-5],
-        [1,1], 
-        [2+1e-5, 2],
-        [1+1e-5, 2],
-        [2, 1],
-        [0,0], 
-        [1,2+1e-5],
-        [2-1e-5, 1],
-        [2, 2],
+    testArray = np.array([
+        [0, 0],
+        [0+1e-6, 0],    # dup, 1 col, 1e-6
+        [0-1e-6, 0],    # dup, 1 col, 1e-6
+        [0+1e-5, 0],    # dup, 1 col, 1e-5
+        [0-1e-5, 0],    # dup, 1 col, 1e-5
+        [0, 0+1e-5],    # dup, 2 col, 1e-5
+        [0, 0-1e-5],    # dup, 2 col, 1e-5
+        [0, 0+1e-6],    # dup, 2 col, 1e-6
+        [0, 0-1e-6],    # dup, 2 col, 1e-6
+        [0.5, 1],
+        [0.5-1e-6, 1],  # Half value edgecase, col 1, 1e-6
+        [0.5+1e-6, 1],  # Half value edgecase, col 1, 1e-6
+        [0.5-1e-5, 1],  # Half value edgecase, col 1, 1e-5
+        [0.5+1e-5, 1],  # Half value edgecase, col 1, 1e-5  
     ])
+    # shuffle array before uniquetol
+    rng = np.random.default_rng()
+    arr = np.arange(testArray.shape[0])
+    rng.shuffle(arr)
+    testArray = testArray[arr,:]
 
-    # uniquetol(a, 1e-4)
-    uniquetol(a, 1e-4)
+    # run uniquetol
+    uniquerows = uniquetol(testArray, 1e-5)
+    print('uniquetol result: {0} entries'.format(uniquerows.shape[0]))
+    print(uniquerows)
 
-
-
+    SETUPCODE="""
+import numpy as np
+from __main__ import uniquetol
+testArray = np.array([
+    [0, 0],
+    [0+1e-6, 0],    # dup, 1 col, 1e-6
+    [0-1e-6, 0],    # dup, 1 col, 1e-6
+    [0+1e-5, 0],    # dup, 1 col, 1e-5
+    [0-1e-5, 0],    # dup, 1 col, 1e-5
+    [0, 0+1e-5],    # dup, 2 col, 1e-5
+    [0, 0-1e-5],    # dup, 2 col, 1e-5
+    [0, 0+1e-6],    # dup, 2 col, 1e-6
+    [0, 0-1e-6],    # dup, 2 col, 1e-6
+    [0.5, 1],
+    [0.5-1e-6, 1],  # Half value edgecase, col 1, 1e-6
+    [0.5+1e-6, 1],  # Half value edgecase, col 1, 1e-6
+    [0.5-1e-5, 1],  # Half value edgecase, col 1, 1e-5
+    [0.5+1e-5, 1],  # Half value edgecase, col 1, 1e-5  
+])
+# shuffle array before uniquetol
+rng = np.random.default_rng()
+arr = np.arange(testArray.shape[0])
+rng.shuffle(arr)
+testArray = testArray[arr,:]
+    """
+    TESTCODE="""
+uniquetol(testArray, 1e-5)
+    """
+    v1time = timeit.timeit(
+        setup=SETUPCODE,
+        stmt=TESTCODE,
+        number=10000
+    )
+    print('uniquetol time for 10000 ={0}'.format(v1time))
